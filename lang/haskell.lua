f1d6639acb01c827ddc502dc5700ef5338f0a82e
FrancoBujakiewicz <franconatahelbuja@gmail.com>
2025-10-23 17:29:12 -0300

    Removing unexistent vars

--- lang/haskell.lua
+++ lang/haskell.lua
@@ -21,16 +21,16 @@
 
  hl("Underlined", { underline = true })
  hl("Ignore", { fg = color })
- hl("Error", { fg = color_lighter, bg = bg, bold = true })
- hl("Todo", { fg = color_lighter, bg = bg_darker, bold = true })
+ hl("Error", { fg = color_light, bold = true })
+ hl("Todo", { fg = color_light, bold = true })
 
  hl("Operator", { fg = color_light3 })
  hl("Exception", { fg = color_light })
 
- hl("Type", { fg = color_lighter, bold = true })
+ hl("Type", { fg = color_light, bold = true })
  hl("StorageClass", { fg = color })
  hl("Structure", { fg = color })
- hl("Typedef", { fg = color_lighter })
+ hl("Typedef", { fg = color_light })
 
  -- Preprocessor
  hl("PreProc", { fg = color_light })
@@ -40,17 +40,17 @@
   
  -- Special characters & markup
  hl("Special", { fg = color_light2 })
- hl("SpecialChar", { fg = color_ligth2 })
+ hl("SpecialChar", { fg = color_light2 })
  hl("Tag", { fg = color_light })
  hl("Delimiter", { fg = color_light3 })
 
- hl("String", { fg = color_dark })
- hl("Character", { fg = color_dark })
- hl("Number", { fg = color_lighter })
- hl("Boolean", { fg = color_lighter })
- hl("Float", { fg = color_lighter })
+ hl("String", { fg = color })
+ hl("Character", { fg = color })
+ hl("Number", { fg = color_light })
+ hl("Boolean", { fg = color_light })
+ hl("Float", { fg = color_light })
 
- hl("Identifier", { fg = color_lighter })
+ hl("Identifier", { fg = color_light })
  hl("Function", { fg = color_light, bold = true })
  hl("Keyword", { fg = color_light, bold = true })
  hl("Statement", { fg = color_light })
